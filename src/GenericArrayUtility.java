816cbafcb5c61d626ac57949517fff598fed27c6
JASPIC-coder <iradukundachristine27@gmail.com>
2025-04-29 16:01:27 +0200

    Generic merge arrays method

--- src/GenericArrayUtility.java
+++ src/GenericArrayUtility.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+
+import java.lang.reflect.Array;
 
 public class GenericArrayUtility {
+    public static <T> T[] mergeArrays(T[] a, T[] b) {
+        int length = a.length + b.length;
+        @SuppressWarnings("unchecked")
+        T[] result = (T[]) Array.newInstance(a.getClass().getComponentType(), length);
+        System.arraycopy(a, 0, result, 0, a.length);
+        System.arraycopy(b, 0, result, a.length, b.length);
+        return result;
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Integer[] array1 = {1, 2, 3, 4};
+        Integer[] array2 = {5, 6, 7, 8};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Integer[] mergedIntegers = GenericArrayUtility.mergeArrays(array1, array2);
+        System.out.println("Merged Integer Array: " + Arrays.toString(mergedIntegers));
+
+        String[] stringArray1 = {"I", "LIKE", "NIKE"};
+        String[] stringArray2 = {"SO", "SO", "MUCH"};
+
+        String[] mergedStrings = GenericArrayUtility.mergeArrays(stringArray1, stringArray2);
+        System.out.println("Merged String Array: " + Arrays.toString(mergedStrings));
     }
 }
+
